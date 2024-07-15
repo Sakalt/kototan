8d1cc908954689092b765c46f68d1d6fea81f99c
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-07-15 14:58:18 +0900

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,114 @@
+const dictionary = [
+    {
+        id: 1,
+        kanji: "彼",
+        hiragana: "あー",
+        meaning: "自分自身を指す語",
+        example: {
+            japanese: "私は元気です。",
+            fictionalLanguage: "彼是の鏡に。",
+            japaneseExample: "あなたは私の友達です。"
+        }
+    },
+    {
+        id: 2,
+        kanji: "是",
+        hiragana: "ぜ",
+        meaning: "格助詞",
+        example: {
+            japanese: "りんごは赤いです。",
+            fictionalLanguage: "",
+            japaneseExample: "これはりんごです。"
+        }
+    },
+    {檎
+        id: 3,
+        kanji: "檎果",
+        hiragana: "こか",
+        meaning: "果物の一種",
+        example: {
+            japanese: "リンゴを食べます。",
+            fictionalLanguage: "こかを。",
+            japaneseExample: "毎朝、リンゴを食べます。"
+        }
+    },
+    {
+        id: 4,
+        kanji: "食べる",
+        hiragana: "",
+        meaning: "格助詞",
+        example: {
+            japanese: "リンゴを食べます。",
+            fictionalLanguage: "きいる。",
+            japaneseExample: "本を読みます。"
+        }
+    },
+    {
+        id: 5,
+        kanji: "過去形",
+        hiragana: "かこけい",
+        meaning: "動詞の過去の形",
+        example: {
+            japanese: "食べました。",
+            fictionalLanguage: "そねねげ。",
+            japaneseExample: "昨日、友達と食べました。"
+        }
+    },
+    {
+        id: 6,
+        kanji: "日",
+        hiragana: "ひ",
+        meaning: "太陽の周りを地球が一周する時間",
+        example: {
+            japanese: "今日は晴れです。",
+            fictionalLanguage: "せえ。",
+            japaneseExample: "今日は何をしますか。"
+        }
+    },
+    {
+        id: 7,
+        kanji: "月",
+        hiragana: "つき",
+        meaning: "地球の衛星",
+        example: {
+            japanese: "今日は満月です。",
+            fictionalLanguage: "ぺいちん。",
+            japaneseExample: "満月の夜はきれいですね。"
+        }
+    },
+    {
+        id: 8,
+        kanji: "火",
+        hiragana: "ひ",
+        meaning: "炎のこと",
+        example: {
+            japanese: "火が燃えています。",
+            fictionalLanguage: "ねんき。",
+            japaneseExample: "焚火が暖かいです。"
+        }
+    },
+    {
+        id: 9,
+        kanji: "水",
+        hiragana: "みず",
+        meaning: "液体の一種",
+        example: {
+            japanese: "水を飲みます。",
+            fictionalLanguage: "ちいん。",
+            japaneseExample: "水を一杯ください。"
+        }
+    },
+    {
+        id: 10,
+        kanji: "木",
+        hiragana: "き",
+        meaning: "木材",
+        example: {
+            japanese: "木が高いです。",
+            fictionalLanguage: "せいい。",
+            japaneseExample: "この家は木でできています。"
+        }
+    },
+];
+
+export default dictionary;
